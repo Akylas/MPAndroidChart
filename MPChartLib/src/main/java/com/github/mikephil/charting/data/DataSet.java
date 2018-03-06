501bbdf56c2ff2eb2611ee3f64ca4b85e81a5a2c
Martin Guillon <martin.guillon@akylas.fr>
2018-03-06 17:34:25 +0100

    check size firrst

--- MPChartLib/src/main/java/com/github/mikephil/charting/data/DataSet.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/data/DataSet.java
@@ -283,7 +283,7 @@ public abstract class DataSet<T extends Entry> extends BaseDataSet<T> {
 
     @Override
     public T getEntryForIndex(int index) {
-        if(index >= 0) {
+        if(index >= 0 && mValues.size() > 0) {
             return mValues.get(index);
         }
         return null;
